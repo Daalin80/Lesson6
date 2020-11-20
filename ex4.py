c3e5539083ef5fe89916f8911c3d0b57981c3598
Daalin80 <73665109+Daalin80@users.noreply.github.com>
2020-11-20 09:02:03 +0200

    Delete ex4.py

--- ex4.py
+++
@@ -1,7 +0,0 @@
-import json
-path = 'acdc.json'
-with open(path, 'r') as f:
-    data = json.loads(f.read())
-    for i in data['album']['tracks']['track']:
-        if i["duration"] > '0':
-            print(i["duration"])
